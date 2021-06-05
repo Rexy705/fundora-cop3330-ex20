30e1f28a24e3dfec05fbe8d98cda7a3ed6838c5e
Rey <reyfundora@icloud.com>
2021-06-05 17:04:03 -0400

    Initial commit

---
+++ src/main/java/ex20/ex20.java
@@ -0,0 +1,63 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Rey Fundora
+ */
+
+package ex20;
+
+import java.util.Scanner;
+
+public class ex20 {
+    public static void main (String[] args) {
+        Scanner input = new Scanner(System.in);
+        Boolean correct = false;
+
+        while (correct.equals(false)) {
+            System.out.print("What is the order amount? ");
+            String order_in = input.nextLine();
+            System.out.print("What state do you live in? ");
+            String state_in = input.nextLine();
+            state_in = state_in.toLowerCase();
+
+            try {
+                double order = Double.parseDouble(order_in);
+                if (state_in.equals("wisconsin") || (state_in.equals("wi"))) {
+                    System.out.print("What is your county? ");
+                    String county_in = input.nextLine();
+                    county_in = county_in.toLowerCase();
+                    county_in = county_in.replaceAll(" ", "_");
+
+                    if (county_in.equals("eau_claire") || county_in.equals("eau_claire_county")) {
+                        double tax = order * .05;
+                        double tax2 = order * .005;
+                        System.out.printf("The tax is $%.2f.\n", tax + tax2);
+                        System.out.printf("The total is $%.2f.\n", order + tax + tax2);
+                    }
+                    if (county_in.equals("dunn") || county_in.equals("dunn_county")) {
+                        double tax = order * .05;
+                        double tax2 = order * .004;
+                        System.out.printf("The tax is $%.2f.\n", tax + tax2);
+                        System.out.printf("The total is $%.2f.\n", order + tax + tax2);
+                    }
+                }
+
+                if (state_in.equals("illinois") || (state_in.equals("il"))) {
+                    double tax = order * .08;
+                    System.out.printf("The tax is $%.2f.\n", tax);
+                    System.out.printf("the total is $%.2f.\n", order + tax);
+                }
+
+                if (!state_in.equals("illinois") && !state_in.equals("il") &&
+                        !state_in.equals("wisconsin") && !state_in.equals("wi")) {
+                    System.out.printf("The total is $%.2f.\n", order);
+                }
+
+                correct = true;
+            }
+            catch (Exception e) {
+                System.out.println("Please only enter numeric values. Try again.\n");
+                correct = false;
+            }
+        }
+    }
+}
